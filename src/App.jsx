674f85dd6286271a4cc3e028b59d94682636172f
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2024-01-24 18:27:08 +0500

    complete game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -32,63 +32,22 @@ export default function Board() {
     ? (status = `${winner} wins!`)
     : (status = `${xIsNext ? "X" : "O"} is next`);
 
+  let cards = [];
+  for (let j = 0; j < 9; j++) {
+    cards.push(
+      <Square
+        value={squares[j]}
+        onSquareClick={() => {
+          handleClick(j);
+        }}
+      />
+    );
+  }
+
   return (
     <>
-      <Square
-        value={squares[0]}
-        onSquareClick={() => {
-          handleClick(0);
-        }}
-      />
-      <Square
-        value={squares[1]}
-        onSquareClick={() => {
-          handleClick(1);
-        }}
-      />
-      <Square
-        value={squares[2]}
-        onSquareClick={() => {
-          handleClick(2);
-        }}
-      />
-      <Square
-        value={squares[3]}
-        onSquareClick={() => {
-          handleClick(3);
-        }}
-      />
-      <Square
-        value={squares[4]}
-        onSquareClick={() => {
-          handleClick(4);
-        }}
-      />
-      <Square
-        value={squares[5]}
-        onSquareClick={() => {
-          handleClick(5);
-        }}
-      />
-      <Square
-        value={squares[6]}
-        onSquareClick={() => {
-          handleClick(6);
-        }}
-      />
-      <Square
-        value={squares[7]}
-        onSquareClick={() => {
-          handleClick(7);
-        }}
-      />
-      <Square
-        value={squares[8]}
-        onSquareClick={() => {
-          handleClick(8);
-        }}
-      />
-      <div className="statuus">{status}</div>
+      {...cards}
+      <div>{status}</div>
     </>
   );
 }
