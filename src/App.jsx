030d94a872d2ad3c9cef550bf30152056af6bddc
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2024-01-24 14:21:46 +0500

    initialize project files
    add tailwindcss, react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <p className="text-9xl">Hi Mom!</p>;
+}
+
+export default App;
